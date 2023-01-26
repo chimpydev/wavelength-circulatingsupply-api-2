9c57470bc689387e6c25c6495a7bb120cf6eb4d4
Chimpy Developer <86309030+chimpydev@users.noreply.github.com>
2023-01-26 19:41:40 +0000

    gaz

---
+++ routes/users.js
@@ -0,0 +1,72 @@
+const {request, gql} = require('graphql-request');
+
+
+const getServerSideProps = async () => {
+
+  
+    const query = gql`
+    {
+      embrGetProtocolData {
+        circulatingSupply
+      }
+    }
+    `;
+  
+  
+    const results = await request('https://teste.testeborabora.cyou/graphql', query);
+    console.log("vai toma: "+JSON.stringify(results.embrGetProtocolData))
+    return results.embrGetProtocolData.circulatingSupply
+    };
+  
+
+const userRoutes = async (app, fs) => {
+    // variables  
+        const query = gql`
+        {
+          embrGetProtocolData {
+            circulatingSupply
+          }
+        }
+        `;
+      
+      
+        const results = await request('https://teste.testeborabora.cyou/graphql', query);
+        console.log("vai toma: "+JSON.stringify(results.embrGetProtocolData))
+        const dataPath = JSON.stringify(results.embrGetProtocolData.circulatingSupply);
+        
+
+        console.log("DATAPIXOTA: "+JSON.stringify(dataPath.replace("circulatingSupply","")))
+
+
+    // helper methods
+    const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    // READ
+    app.get('/users', (req, res) => {
+        console.log("FRANCISCO: "+JSON.stringify( dataPath))
+       /* fs.readFile(JSON.stringify(dataPath), 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        });*/
+      /*  const d1 = JSON.stringify(dataPath).replace("circulatingSupply","");
+        const d2 = d1.replace("{","");
+        const d3 = d2.replace(/'/g,'');
+        console.log("JOANA: "+d3)*/
+
+        res.send(JSON.parse(dataPath));
+    });
+
+};
+
+module.exports = userRoutes;
